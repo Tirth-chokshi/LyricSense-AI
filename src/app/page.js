05a2039a93c5da66bfc96bce6ff5cab6ad9d038d
Tirth chokshi <chokshitirth4@gmail.com>
2024-07-21 19:20:40 +0530

    clean code++

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from 'react'
 import axios from 'axios'
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -9,18 +8,7 @@ import Keywords from "@/components/Keywords"
 import Analysis from "@/components/Analysis"
 import ChatInterface from '@/components/ChatInterface'
 import { ComingSoonSection } from '@/components/ComingSoonSection'
-// import EmotionGraph from '@/components/EmotionGraph'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-// import WordCloud from '@/components/WordCloud'
-// import SentimentTimeline from '@/components/SentimentTimeline'
-// import RhymeScheme from '@/components/RhymeScheme'
-// import InteractiveLyrics from '@/components/InteractiveLyrics'
-// import ThemeRadar from '@/components/ThemeRadar'
+import BTheme from '@/components/BTheme'
 
 export default function Home() {
   const [songTitle, setSongTitle] = useState('')
@@ -82,20 +70,7 @@ export default function Home() {
             LyricSense AI
           </h1>
           <div className="flex-grow flex justify-end">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className="rounded-full">
-                  <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <BTheme/>
           </div>
         </header>
 
