d4ea134bc750779e442450a1379c17bbb8edb3ed
Tirth chokshi <chokshitirth4@gmail.com>
2024-07-27 00:24:08 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import Head from 'next/head'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/lab-flask.svg" type="image/svg+xml" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
--- src/components/ComingSoonSection.jsx
+++ src/components/ComingSoonSection.jsx
@@ -17,11 +17,11 @@ const FeatureCard = ({ title, description, icon }) => (
 
 export const ComingSoonSection = () => {
   const features = [
-    { 
-      title: "Dynamic Song Search", 
-      description: "Search for songs dynamically as you type",
-      icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    },
+    // { 
+    //   title: "Dynamic Song Search", 
+    //   description: "Search for songs dynamically as you type",
+    //   icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    // },
     { 
       title: "Popular Songs", 
       description: "See current popular song's insights",
