b57c1c982bb47c38c33a3c3bcd4fcc38804ac28b
Tirth chokshi <chokshitirth4@gmail.com>
2024-07-29 08:25:13 +0530

    icon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-import Head from 'next/head'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "LyricSense AI",
   description: "Get your favorite songs analytics",
+  icons: {
+    icon: '/lab-flask.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/lab-flask.svg" type="image/svg+xml" />
-      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
