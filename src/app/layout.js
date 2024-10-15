e9cc81d9e21cc39d37404b759af65ba8ebc02d9d
Tirth chokshi <chokshitirth4@gmail.com>
2024-10-15 12:38:28 +0530

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           {children}
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
